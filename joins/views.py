da61eb33de92903a8aaded10ae05b34b7188b938
ArchAiA <xarchaiax@gmail.com>
2015-07-01 23:11:57 -0400

    L25

--- joins/views.py
+++ joins/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .forms import EmailForm, JoinForm
 from .models import Join
 
+import uuid
 
 """L24: GET IP"""
 def get_ip(request):
@@ -17,6 +18,14 @@ def get_ip(request):
 	return ip
 """L24: GET IP"""
 
+
+def get_ref_id():
+	ref_id = str(uuid.uuid4())[:11].replace('-','').lower()
+	try:
+		id_exists = Join.objects.get(ref_id=ref_id)
+		get_ref_id()
+	except:
+		return ref_id
 
 
 
@@ -37,18 +46,19 @@ def home(request):
 
 	
 	"""USING MODEL FORMS"""
-	form = JoinForm(request.POST or None)
+	form = JoinForm(request.POST or None) #this creates an instance of JoinForm called form
 	if form.is_valid():
 		new_join = form.save(commit=False) #The commit statement allows us to modify before saving data
 		# #We might do something here
-		email = form.cleaned_data['email']
-		new_join_old, created = Join.objects.get_or_create(email=email)
-		if create:
-			new_join_old.ip_address = get_ip(request)
+		email = form.cleaned_data['email'] #this takes the email from the form for the creation of new_join_old in the next line
+		new_join_old, created = Join.objects.get_or_create(email=email) #This creates an instance of Join and uses the email field from the form
+		if created: #if the email already exists, the attributes of the new instance of Join are set below
+			new_join_old.ref_id = get_ref_id()
+			new_join_old.ip_address = get_ip(request) #get ip address
 			new_join_old.save()
 		#redirect here
 
-		# new_join.ip_address=get_ip(request)
+		# new_join.ip_address=get_ip(request) #get ip address
 		# new_join.save()		
 	"""USING MODEL FORMS"""
 
